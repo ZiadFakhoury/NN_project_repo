3ba4e1eec33bae280a4aeb98d6d4946a82812546
Ziad Fakhoury <ziad.fakhoury@hotmail.com>
2019-08-12 17:44:13 +0400

    Continued to develop MLP network class and trainer class

--- MLP_Class.py
+++ MLP_Class.py
@@ -86,7 +86,7 @@ class ConnectedLayerLink:
                 "np,pk -> nk", self.weights, prev_layer.neurons)))
 
     def dnext_dprev(self, prev_layer):
-        """Returns 3 - dimensional array matrix derivatives between next_layer
+        """Returns 3 - dimensional numpy array matrix derivatives between next_layer
         and prev_layer in different examples
 
         Parameters:
@@ -96,6 +96,18 @@ class ConnectedLayerLink:
                          self.dactivate(
                              np.einsum("np,pk -> nk", self.weights, prev_layer.neurons)),
                          self.weights)
+
+    def dnext_dweight(self, prev_layer):
+        """Return 3 - dimensional numpy array matrix derivative between next_layer and
+        the weights between prev_layer and next_layer in different examples
+
+        Parameters:
+        prev_layer(Layer): Previous layer in LayerLink
+        """
+        return np.einsum("nk,pk -> npk",
+                         self.dactivate(
+                             np.einsum("np,pk -> nk", self.weights, prev_layer.neurons)),
+                         prev_layer.neurons)
 
 
 class MLP:
@@ -113,6 +125,14 @@ class MLP:
         self.output_layer = Layer(output_size)
         self.layers = [self.input_layer, self.output_layer]
         self.links = []
+        self.neuron_neuron_derivatives = []
+        self.neuron_weight_derivatives = []
+        self.output_weight_derivatives = []
+
+    def reset_derivatives(self):
+        self.neuron_neuron_derivatives = []
+        self.neuron_weight_derivatives = []
+        self.output_weight_derivatives = []
 
     def normal_dist(self, mean=0, stdev=1):
         """Assigns Weights to random number in a normal distribution
@@ -140,25 +160,71 @@ class MLP:
                 ConnectedLayerLink(self.layers[x], self.layers[x+1]))
 
     def calc_neurons(self, input_data):
-        """"""
+        """Calculates Neurons and uploads them onto to network.
+
+        Parameters:
+        input_data(np-arrary): Input data in shape (neurons, examples)
+        """
         example_number = input_data.shape[-1]
         for layer in self.layers:
             layer.reset_layer(examples=example_number)
         for x in range(len(self.links)):
             self.links[x].propogate(self.layers[x],self.layers[x+1])
 
+    def compute_neuron_neuron_derivative(self):
+        """Calculates derivatives between layers of all links and places them in
+        a derivative_array attribute"""
+        for x in range(len(self.links)):
+            self.neuron_neuron_derivatives.append(self.links[x].dnext_dprev(self.layers[x]))
+
+    def compute_neuron_weight_derivative(self):
+        """Calculates derivatives between next_layer and weights for all layers links ad stores them
+        in a neuron_weight_derivative_array attribute"""
+        for x in range(len(self.links)):
+            self.neuron_weight_derivatives.append(self.links[x].dnext_dweight(self.layers[x]))
+
+    def compute_output_weight_derivative(self):
+        factor = self.neuron_neuron_derivatives[-1]
+        self.output_weight_derivatives.append(self.neuron_weight_derivatives[-1])
+        for x in range(2, len(self.links)+ 1):
+            self.output_weight_derivatives.append(
+                np.einsum("npk,mnk -> mnpk",
+                          self.neuron_weight_derivatives[-x], factor))
+            factor = np.einsum("ijk,jhk -> ihk", factor, self.neuron_neuron_derivatives[-x])
+        self.output_weight_derivatives.reverse()
 
 
 class Trainer:
 
-    def __init__(self,loss_func=func.quadratic_loss):
+    def __init__(self, loss_func=func.quadratic_loss, dloss_func=func.v_dquadratic_loss):
         """Training algorithm used on network. Defaults to standard MSE loss
 
         Parameters:
         loss_func(func, optional): Loss Function used, defaults to quadratic_loss
         """
         self.loss = loss_func
+        self.dloss = dloss_func
 
+    def train_network_once(self, network, examples_input_data, learning_rate, examples_output_data):
+        network.calc_neurons(examples_input_data)
+        network.compute_neuron_neuron_derivative()
+        network.compute_neuron_weight_derivative()
+        network.compute_outpute_weight_derivative()
 
-    def train_network(self, network, examples):
+        cost = 0
+        for i in range(examples_output_data.shape[-1]):
+            cost += self.loss(examples_output_data[:, i], network.neurons[-1][:, i])
+        cost = cost/examples_output_data.shape[-1]
+        print(cost)
 
+        a = self.dloss(examples_output_data, network.neurons[-1])
+        for x in range(len(network.links)):
+            network.links[x].weights -= learning_rate*np.einsum("ik,inpk -> np",
+                                                                a,
+                                                                network.output_weight_derivatives[x])/examples_input_data.shape[-1]
+
+        cost = 0
+        for i in range(examples_output_data.shape[-1]):
+            cost += self.loss(examples_output_data[:, i], network.neurons[-1][:, i])
+        cost = cost / examples_output_data.shape[-1]
+        print(cost)
--- Mathematical_Functions.py
+++ Mathematical_Functions.py
@@ -13,5 +13,10 @@ def quadratic_loss(actual, expected):
     return (np.sum((actual - expected))**2)/np.size(actual)
 
 
+def dquadratic_loss(actual, expected):
+    return -2*(actual-expected)/np.size(actual)
+
+
 v_sigmoid = np.vectorize(sigmoid)
 v_dsigmoid = np.vectorize(dsigmoid)
+v_dquadratic_loss = np.vectorize(dquadratic_loss)
